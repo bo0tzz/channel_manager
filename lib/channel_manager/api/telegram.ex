ee644c64e67821ae67fa94b35b6f7bc12d9a9510
bo0tzz <btdewilligen@gmail.com>
2021-07-10 15:36:01 +0200

    Move telegram target api details into separate Api namespace

---
+++ lib/channel_manager/api/telegram.ex
@@ -0,0 +1,35 @@
+defmodule ChannelManager.Api.Telegram do
+  alias ChannelManager.Model.Post
+
+  require Logger
+
+  @bot :channel_manager
+
+  use ExGram.Bot,
+    name: @bot,
+    setup_commands: true
+
+  def bot(), do: @bot
+  def me(), do: ExGram.get_me(bot: bot())
+
+  middleware(ExGram.Middleware.IgnoreUsername)
+
+  def send_post(post, target, true), do: send_post(post, target)
+  def send_post(post, target, false), do: send_post(%Post{post | caption: ""}, target)
+
+  def send_post(%Post{type: "link", url: url, caption: caption} = post, target) do
+    message = caption <> "\n" <> url
+
+    case ExGram.send_message(target, message, bot: bot()) do
+      {:error, e} -> Logger.error("Failed to send post #{inspect(post)}: #{inspect(e)}")
+      {:ok, _} -> nil
+    end
+  end
+
+  def send_post(%Post{type: "image", url: url, caption: caption} = post, target) do
+    case ExGram.send_photo(target, url, bot: bot(), caption: caption) do
+      {:error, e} -> Logger.error("Failed to send post #{inspect(post)}: #{inspect(e)}")
+      {:ok, _} -> nil
+    end
+  end
+end
--- lib/channel_manager/forwarder/target/telegram.ex
+++ lib/channel_manager/forwarder/target/telegram.ex
@@ -5,37 +5,7 @@ defmodule ChannelManager.Forwarder.Target.Telegram do
   @behaviour Target
 
   @impl Target
-  def send(
-        %Target{type: "telegram", target: target, options: options},
-        %ChannelManager.Model.Post{type: "image", url: url, caption: caption} = post
-      ) do
-    caption =
-      case Map.get(options, "captions", false) do
-        true -> caption
-        false -> ""
-      end
-
-    case ExGram.send_photo(target, url, bot: ChannelManager.bot(), caption: caption) do
-      {:error, e} -> Logger.error("Failed to send post #{inspect(post)}: #{inspect(e)}")
-      {:ok, _} -> nil
-    end
-  end
-
-  def send(
-        %Target{type: "telegram", target: target, options: options},
-        %ChannelManager.Model.Post{type: "link", url: url, caption: caption} = post
-      ) do
-    caption =
-      case Map.get(options, "captions", false) do
-        true -> caption
-        false -> ""
-      end
-
-    message = caption <> "\n" <> url
-
-    case ExGram.send_message(target, message, bot: ChannelManager.bot()) do
-      {:error, e} -> Logger.error("Failed to send post #{inspect(post)}: #{inspect(e)}")
-      {:ok, _} -> nil
-    end
+  def send(%Target{type: "telegram", target: target, options: options}, post) do
+    ChannelManager.Api.Telegram.send_post(post, target, Map.get(options, "captions", false))
   end
 end
